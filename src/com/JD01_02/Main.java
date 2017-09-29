e55530485dc4a19f144d89b363698964d7677cb8
kabandos <wpsiteguard@gmail.com>
2017-09-29 19:19:34 +0300

    Task A

--- src/com/JD01_02/Main.java
+++ src/com/JD01_02/Main.java
@@ -1,8 +1,25 @@
 package com.JD01_02;
 
-/**
- * Created by kaban on 29.09.2017.
- */
+import java.io.IOException;
+
 public class Main {
+
+    public static void main(String[] args) throws IOException {
+        String line = Util.getOneLine();
+        String[] strMas = line.split(" ");
+        int[] intMas = new int[strMas.length];
+        for(int j = 0; j < strMas.length; j++){
+            intMas[j] = Integer.parseInt(strMas[j]);
+        }
+        int max = TaskA.getMax(intMas);
+        System.out.println("Max : " + max);
+        int min = TaskA.getMin (intMas);
+        System.out.println("Min : " + min);
+        int arifm = TaskA.getArifm(intMas);
+        System.out.println("Srednee arifmeticheskoe ravno "+arifm);
+        int povtor = TaskA.getPovtor(intMas);
+        System.out.println("Povtori : "+povtor);
+    }
+
 
 }
---
+++ src/com/JD01_02/Util.java
@@ -0,0 +1,18 @@
+package com.JD01_02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by kaban on 29.09.2017.
+ */
+class Util {
+
+    static String getOneLine () throws IOException{
+        try(BufferedReader stin = new BufferedReader(new InputStreamReader(System.in))) {
+            return stin.readLine();
+        }
+
+    }
+}
