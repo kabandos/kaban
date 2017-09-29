0c1a0eb2a63cb901e5868c9bbef28fc831221217
kabandos <wpsiteguard@gmail.com>
2017-09-29 21:14:39 +0300

    Int

---
+++ src/com/jd01_04/Main.java
@@ -0,0 +1,26 @@
+package com.jd01_04;
+
+import java.io.IOException;
+
+/**
+ * Created by kaban on 29.09.2017.
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        for (int i = 2; i<10; i++) {
+            for (int j = 2; j < 10; j++) {
+                System.out.print(j+" * " +i+" = "+(i*j)+ "\t");
+            }
+            System.out.println();
+        }
+//        String[] cols = Utils.getCols();
+        String[] cols = "4 0 3 1 2 ".split(" ");
+        Utils.writeCols(cols);
+        String [] sorted = Utils.sort(cols);
+        Utils.writeCols(sorted);
+        int firstIndex = Utils.calcIndex(sorted, cols[0]);
+        int lastIndex = Utils.calcIndex (sorted, cols[cols.length-1]);
+        System.out.println("\nPerviy bil, stal: "+firstIndex);
+        System.out.println("\nPosledniy bil, stal: "+lastIndex);
+    }
+}
